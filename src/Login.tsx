1eb75db3cf5fb7c3a08e2585015ca852d4499d35
Brett Adams <brettadams@fastmail.com>
2020-12-28 09:10:37 -0500

    migrate registration and login to react-hook-form

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,4 @@
-import firebase from "firebase/app"
+import { useForm } from "react-hook-form"
 import { useHistory } from "react-router-dom"
 import {
   EuiButton,
@@ -12,107 +12,125 @@ import {
   EuiSpacer,
   EuiText,
 } from "@elastic/eui"
-import { Form, Formik } from "formik"
-import React, { useEffect, useState } from "react"
-
-// import { useObservableState } from "observable-hooks"
-// import { EMPTY } from "rxjs"
-// import { catchError, map, switchMap, tap } from "rxjs/operators"
-// import { useAuth0 } from "@auth0/auth0-react"
+import React, { useEffect } from "react"
 
 import { useAuth } from "./lib/auth"
-// import { me, currentUser$, currentOrganization$ } from "./lib/user"
-//
-interface LoginValues {
+import { useFieldErrorMessages } from "./lib/validation"
+
+interface FormValues {
   email: string
   password: string
 }
 
 export const Login: React.FC = () => {
-  console.log("Login()")
-  // const isLoggedIn = useObservableState(isLoggedIn$)
   const history = useHistory()
-  const [errorMessage, setErrorMessage] = useState("")
   const { login, user } = useAuth()
-  // const { isAuthenticated, loginWithRedirect } = useAuth0()
+
+  const {
+    errors,
+    formState: { isSubmitting, isValid },
+    handleSubmit,
+    register,
+    setError,
+  } = useForm<FormValues>({
+    defaultValues: { email: "", password: "" },
+    mode: "onChange",
+  })
 
   useEffect(() => {
-    console.log("** user changed **")
-    console.log(!!user)
     if (user) {
       history.push("/")
     }
   }, [user])
-  // console.log(`Login.isAuthenticated: ${isAuthenticated}`)
-  // if (isAuthenticated) {
-  //   history.replace("/search")
-  // }
-  //
-  // console.log("loginWithRedirect()")
 
-  const onSubmit = async (values: LoginValues) => {
-    // setErrorMessage("")
-    console.log("onSubmit")
+  const getFieldErrorMessage = useFieldErrorMessages(
+    {
+      email: {
+        "auth/invalid-email": "Invalid email",
+        "auth/user-not-found": "User not found",
+        required: "Required",
+      },
+      password: {
+        "auth/wrong-password": "Wrong password",
+        required: "Required",
+      },
+    },
+    errors,
+  )
+
+  const onSubmit = async (values: FormValues) => {
     try {
       const resp = await login(values.email, values.password)
       console.log(resp)
       history.push("/")
     } catch (e) {
-      // TODO: handle error
+      // TODO: handle more firebase error codes
       console.log(e)
+      if (e.code) {
+        const field = e.code === "auth/wrong-password" ? "password" : "email"
+        setError(field, {
+          type: e.code,
+          shouldFocus: true,
+        })
+      } else {
+        setError("email", {
+          type: "auth/unknown",
+          shouldFocus: true,
+        })
+      }
     }
   }
 
   return (
-    <Formik
-      enableReinitialize={true}
-      initialValues={{ email: "", password: "" }}
-      onSubmit={onSubmit}
-    >
-      {({ values, handleChange, handleSubmit }) => (
-        <EuiFlexGroup alignItems="center">
-          <EuiFlexItem style={{ alignItems: "center" }}>
-            <EuiForm
-              style={{ padding: "2rem", minWidth: "400px" }}
-              component="form"
-              onSubmit={handleSubmit}
-            >
-              <EuiText>
-                <p style={{ marginBottom: "0" }}>Welcome to</p>
-                <h1 style={{ marginTop: "0" }}>Sepal</h1>
-              </EuiText>
-              <EuiFormRow label="Email" style={{ marginTop: "1rem" }}>
-                <EuiFieldText name="email" value={values.email} onChange={handleChange} />
-              </EuiFormRow>
-              <EuiFormRow label="Password">
-                <EuiFieldPassword
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                />
-              </EuiFormRow>
-              <EuiSpacer />
-              <EuiButton type="submit" fill>
-                Sign In
-              </EuiButton>
-              <EuiLink href="/register" style={{ marginLeft: "20px", fontSize: "80%" }}>
-                Don't have an account?
-              </EuiLink>
-              {errorMessage && (
-                <div>
-                  <EuiText color="danger">{errorMessage}</EuiText>
-                </div>
-              )}
-            </EuiForm>
-          </EuiFlexItem>
-          <EuiFlexItem grow={2}>
-            <div className="Login--splash-img" style={{ minHeight: "100vh" }}>
-              {" "}
-              &nbsp;
-            </div>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      )}
-    </Formik>
+    <EuiFlexGroup alignItems="center">
+      <EuiFlexItem style={{ alignItems: "center" }}>
+        <EuiForm
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          style={{ padding: "2rem", minWidth: "400px" }}
+        >
+          <EuiText>
+            <p style={{ marginBottom: "0" }}>Welcome to</p>
+            <h1 style={{ marginTop: "0" }}>Sepal</h1>
+          </EuiText>
+          <EuiFormRow
+            error={getFieldErrorMessage("email")}
+            isInvalid={!!errors.email}
+            label="Email"
+            style={{ marginTop: "1rem" }}
+          >
+            <EuiFieldText
+              name="email"
+              inputRef={register({ required: true, validate: () => true })}
+              isInvalid={!!errors.email}
+            />
+          </EuiFormRow>
+          <EuiFormRow
+            error={getFieldErrorMessage("password")}
+            isInvalid={!!errors.password}
+            label="Password"
+          >
+            <EuiFieldPassword
+              name="password"
+              inputRef={register({ required: true, validate: () => true })}
+              isInvalid={!!errors.password}
+            />
+          </EuiFormRow>
+          <EuiSpacer />
+          <EuiButton type="submit" fill isLoading={isSubmitting} isDisabled={!isValid}>
+            Sign In
+          </EuiButton>
+          <EuiLink href="/register" style={{ marginLeft: "20px", fontSize: "80%" }}>
+            Don't have an account?
+          </EuiLink>
+        </EuiForm>
+      </EuiFlexItem>
+      <EuiFlexItem grow={2}>
+        <div className="Login--splash-img" style={{ minHeight: "100vh" }}>
+          {" "}
+          &nbsp;
+        </div>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   )
 }
--- src/Register.tsx
+++ src/Register.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useForm } from "react-hook-form"
 import { useHistory } from "react-router-dom"
 import {
   EuiButton,
@@ -12,22 +13,51 @@ import {
   EuiSpacer,
   EuiText,
 } from "@elastic/eui"
-import { Formik } from "formik"
 import { signup } from "./lib/auth"
 import { accept as accept_invitation } from "./lib/invitation"
 import { useInitUser, useSearchParams } from "./hooks"
+import { emailRx, useFieldErrorMessages } from "./lib/validation"
 
 interface FormValues {
   email: string
   password: string
   confirmPassword: string
 }
+
 export const Register: React.FC = () => {
   const history = useHistory()
   const searchParams = useSearchParams()
   // TODO: if we have the email we can prefill the email field
   const invitation = searchParams.get("invitation")
   const initUser = useInitUser()
+  const {
+    errors,
+    formState: { isSubmitting, isValid },
+    getValues,
+    handleSubmit,
+    register,
+    setError,
+  } = useForm<FormValues>({
+    defaultValues: { email: "", password: "", confirmPassword: "" },
+    mode: "onChange",
+  })
+
+  const getFieldErrorMessage = useFieldErrorMessages(
+    {
+      email: {
+        pattern: "Invalid email",
+        "auth/email-already-in-use": "Email address already in use",
+      },
+      password: {
+        minLength: "The password must be at least 8 characters long",
+        required: "Required",
+      },
+      confirmPassword: {
+        default: "Should match the password",
+      },
+    },
+    errors,
+  )
 
   const onSubmit = async (values: FormValues) => {
     try {
@@ -39,96 +69,92 @@ export const Register: React.FC = () => {
       }
       history.push("/")
     } catch (e) {
-      // TODO
+      // TODO: handle more firebase error codes
       console.log(e)
+      if (e.code === "auth/email-already-in-use") {
+        setError("email", {
+          type: e.code,
+          shouldFocus: true,
+        })
+      } else {
+        setError("email", {
+          type: "auth/unknown",
+          shouldFocus: true,
+        })
+      }
     }
   }
 
   return (
-    <Formik
-      enableReinitialize={true}
-      initialValues={{
-        username: "",
-        email: "",
-        firstName: "",
-        lastName: "",
-        password: "",
-        confirmPassword: "",
-      }}
-      onSubmit={onSubmit}
-    >
-      {({ values, handleChange, handleSubmit }) => (
-        <EuiFlexGroup alignItems="center">
-          <EuiFlexItem style={{ alignItems: "center" }}>
-            <EuiForm
-              style={{ padding: "2rem", minWidth: "400px" }}
-              component="form"
-              onSubmit={handleSubmit}
-            >
-              <EuiText>
-                <h2 style={{ marginBottom: "0" }}>Create an account</h2>
-              </EuiText>
-              {false && (
-                <EuiFormRow label="Username" style={{ marginTop: "1rem" }}>
-                  <EuiFieldText
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                  />
-                </EuiFormRow>
-              )}
-              <EuiFormRow label="Email">
-                <EuiFieldText name="email" value={values.email} onChange={handleChange} />
-              </EuiFormRow>
-              {false && (
-                <>
-                  <EuiFormRow label="First name">
-                    <EuiFieldText
-                      name="firstName"
-                      value={values.firstName}
-                      onChange={handleChange}
-                    />
-                  </EuiFormRow>
-                  <EuiFormRow label="Last name">
-                    <EuiFieldText
-                      name="lastName"
-                      value={values.lastName}
-                      onChange={handleChange}
-                    />
-                  </EuiFormRow>
-                </>
-              )}
-              <EuiFormRow label="Password">
-                <EuiFieldPassword
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                />
-              </EuiFormRow>
-              <EuiFormRow label="Confirm password">
-                <EuiFieldPassword
-                  name="confirmPassword"
-                  value={values.confirmPassword}
-                  onChange={handleChange}
-                />
-              </EuiFormRow>
-              <EuiSpacer />
-              <EuiButton type="submit" fill>
-                Signup
-              </EuiButton>
-              <EuiLink href="/signup" style={{ marginLeft: "20px", fontSize: "80%" }}>
-                Already have an account?
-              </EuiLink>
-            </EuiForm>
-          </EuiFlexItem>
-          <EuiFlexItem grow={2}>
-            <div className="Login--splash-img" style={{ minHeight: "100vh" }}>
-              {" "}
-              &nbsp;
-            </div>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      )}
-    </Formik>
+    <EuiFlexGroup alignItems="center">
+      <EuiFlexItem style={{ alignItems: "center" }}>
+        <EuiForm
+          style={{ padding: "2rem", minWidth: "400px" }}
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <EuiText>
+            <h2 style={{ marginBottom: "0" }}>Create an account</h2>
+          </EuiText>
+
+          <EuiText>{!!errors.confirmPassword?.message}</EuiText>
+          <EuiFormRow
+            error={getFieldErrorMessage("email")}
+            isInvalid={!!errors.email}
+            label="Email"
+          >
+            <EuiFieldText
+              name="email"
+              inputRef={register({
+                required: true,
+                pattern: emailRx,
+              })}
+              isInvalid={!!errors.email}
+            />
+          </EuiFormRow>
+          <EuiFormRow
+            error={getFieldErrorMessage("password")}
+            isInvalid={!!errors.password}
+            label="Password"
+          >
+            <EuiFieldPassword
+              name="password"
+              inputRef={register({
+                minLength: 8,
+                required: true,
+              })}
+              isInvalid={!!errors.password}
+            />
+          </EuiFormRow>
+          <EuiFormRow
+            error={getFieldErrorMessage("confirmPassword")}
+            isInvalid={!!errors.confirmPassword}
+            label="Confirm password"
+          >
+            <EuiFieldPassword
+              name="confirmPassword"
+              inputRef={register({
+                validate: (value) => value === getValues("password"),
+                required: true,
+              })}
+              isInvalid={!!errors.confirmPassword}
+            />
+          </EuiFormRow>
+          <EuiSpacer />
+          <EuiButton type="submit" fill isLoading={isSubmitting} isDisabled={!isValid}>
+            Signup
+          </EuiButton>
+          <EuiLink href="/login" style={{ marginLeft: "20px", fontSize: "80%" }}>
+            Already have an account?
+          </EuiLink>
+        </EuiForm>
+      </EuiFlexItem>
+      <EuiFlexItem grow={2}>
+        <div className="Login--splash-img" style={{ minHeight: "100vh" }}>
+          {" "}
+          &nbsp;
+        </div>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   )
 }
